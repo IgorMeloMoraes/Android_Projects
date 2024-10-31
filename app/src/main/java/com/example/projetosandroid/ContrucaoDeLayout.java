fa03033f5d55212d243dd8cb7a222afa7ffce617
IgorMeloMoraes <igor-batista@outlook.com.br>
2024-10-31 17:53:29 -0300

    Projeto Layout Noticias

---
+++ app/src/main/java/com/example/projetosandroid/ContrucaoDeLayout.java
@@ -0,0 +1,14 @@
+package com.example.projetosandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContrucaoDeLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contrucao_de_layout);
+    }
+}
