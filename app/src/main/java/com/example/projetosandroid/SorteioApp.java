9b68b62c8fd6461c4200c853685d97b3c3dbcf6c
IgorMeloMoraes <igor-batista@outlook.com.br>
2024-10-31 17:26:24 -0300

    Projeto Sorteio utilizando Random

--- app/src/main/java/com/example/projetosandroid/SorteioApp.java
+++ app/src/main/java/com/example/projetosandroid/SorteioApp.java
@@ -3,12 +3,34 @@ package com.example.projetosandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class SorteioApp extends AppCompatActivity {
+
+    TextView txtNumeroSorteado;
+    Button btnSortear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sorteio_app);
+
+        // Referecia dos Elementos
+        txtNumeroSorteado = findViewById(R.id.txt_num_sorteado);
+        btnSortear = findViewById(R.id.btn_sortear);
+
+        // Evento de Click utilizando o setOnClickListener para captar quando o botão for acionado
+        btnSortear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Gera um numero aleatorio utilizando a classe Random
+                int numeroAleatorio = new Random().nextInt(51);
+                txtNumeroSorteado.setText("Número Sorteado: " + numeroAleatorio);
+            }
+        });
     }
 }
