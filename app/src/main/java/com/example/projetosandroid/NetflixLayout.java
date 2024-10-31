fad1926e7143af7537d83eefdc7af29eed823808
IgorMeloMoraes <igor-batista@outlook.com.br>
2024-10-31 17:32:52 -0300

    Projeto Sorteio Layout Netflix

---
+++ app/src/main/java/com/example/projetosandroid/NetflixLayout.java
@@ -0,0 +1,14 @@
+package com.example.projetosandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NetflixLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_netflix_layout);
+    }
+}
