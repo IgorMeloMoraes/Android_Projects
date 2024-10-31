e354a630e4019d7dcd052a5a831fd92df4cc2619
IgorMeloMoraes <igor-batista@outlook.com.br>
2024-10-31 17:19:04 -0300

    Inicializando Pasta doProjeto no GitHub

---
+++ app/src/main/java/com/example/projetosandroid/SorteioApp.java
@@ -0,0 +1,14 @@
+package com.example.projetosandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SorteioApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sorteio_app);
+    }
+}
